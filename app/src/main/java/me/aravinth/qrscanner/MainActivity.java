e53a67bb8bb0fdca566e3f32c7d9213367a57736
imaravin <aravinththangasami@gmail.com>
2016-01-13 17:18:25 +0530

    first commit

---
+++ app/src/main/java/me/aravinth/qrscanner/MainActivity.java
@@ -0,0 +1,67 @@
+package me.aravinth.qrscanner;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends Activity {
+    Button btn;
+    public IntentIntegrator intentIntegrator;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn=(Button)findViewById(R.id.btn);
+        intentIntegrator =new IntentIntegrator(this);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            //
+                intentIntegrator.initiateScan();
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult intentResult=IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        String str=intentResult.getContents();
+        if(str!=null)
+            Toast.makeText(getApplicationContext(),str,Toast.LENGTH_SHORT);
+        else
+            Toast.makeText(getApplicationContext(),"Failed",Toast.LENGTH_SHORT);
+    }
+}
